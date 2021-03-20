e2e258f4b93b5bef132bb01376accf620ddc2acf
lml17 <lml17@sfu.ca>
2021-03-20 13:39:08 -0700

    lightgbm works

--- Milestone2/src/main.py
+++ Milestone2/src/main.py
@@ -8,10 +8,10 @@ import os
 import numpy as np
 import lightgbm as lgb
 from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import LabelEncoder
 import pickle
 
 import RandomForests, LightGbm
-
 
 def main():
     directory = os.path.dirname('../models/')
@@ -24,24 +24,35 @@ def main():
                 'country_filled','Confirmed', 'Deaths', 'Recovered','Active',
                 'Incidence_Rate', 'Case-Fatality_Ratio']]
 
-    # One hot encoding for categorical values
-    category_clean = pd.get_dummies(all_data)
+    # # Random Forest Classification
+    # # One hot encoding for categorical values
+    # category_clean = pd.get_dummies(all_data)
 
-    # Attach outcome column back
-    category_clean['outcome'] = df['outcome']
+    # # Attach outcome column back
+    # category_clean['outcome'] = df['outcome']
 
-    print("Splitting data into training and validation sets")
-    train, validate = train_test_split(category_clean, test_size=0.2, random_state=42, shuffle=True)
+    # print("Splitting data into training and validation sets")
+    # train, validate = train_test_split(category_clean, test_size=0.2, random_state=42, shuffle=True)
 
-    train_attr = train.drop(columns=['outcome']) # Features
-    train_outcomes = train[['outcome']]
+    # v_data = validate.drop(columns=['outcome'])
+    # v_outcomes = validate[['outcome']]
+    # print("Training Random Forests")
+    # RandomForests.rf_train(train_attr, train_outcomes)
+    # print("Evaluating Random Forests")
+    # RandomForests.rf_eval(v_data,v_outcomes)
 
-    v_data = validate.drop(columns=['outcome'])
-    v_outcomes = validate[['outcome']]
-    print("Training Random Forests")
-    RandomForests.rf_train(train_attr, train_outcomes)
-    print("Evaluating Random Forests")
-    RandomForests.rf_eval(v_data,v_outcomes)
+    # LightGBM Classification
+    y = df[['outcome']]
+    le = LabelEncoder()
+    le.fit(y)
+    y_encoded = le.transform(y)
+
+    X_train, X_valid, y_train, y_valid = train_test_split(all_data, y_encoded, test_size=0.2, random_state=42, shuffle=True)
+
+    # print("Training LightGBM")
+    # LightGbm.boosted_train(X_train, y_train)
+    print("Evaluating LightGBM")
+    LightGbm.boosted_eval(X_valid, y_valid)
 
 if __name__ == '__main__':
     main()
