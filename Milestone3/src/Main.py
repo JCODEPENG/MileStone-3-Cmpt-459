d4fd4aa76dfef9b2f2686fc2ebcd38a73a0607cb
Joshua Peng <jpa95@sfu.ca>
2021-04-18 22:35:54 -0700

    RandomsearchCv

---
+++ Milestone3/src/Main.py
@@ -0,0 +1,137 @@
+# CMPT459 Data Mining
+# Spring 2021 Milestone 2
+# Joshua Peng & Lucia Schmidt
+
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import os
+from sklearn.preprocessing import LabelEncoder
+import matplotlib.pyplot as plt
+
+import RandomForests
+
+
+def main():
+    directory = os.path.dirname('../models/')
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    df = pd.read_csv('./data/cases_train_processed.csv')
+
+    random_forest(df)
+
+def random_forest(df):
+
+    # Temporary grid for trying out values
+    param_grid = {
+        "max_depth": [6, 8],
+        "min_samples_leaf": [2],
+        "n_estimators": [100]
+    }
+
+
+    all_data = df[['age_filled', 'filled_sex', 'province_filled',
+                'country_filled','Confirmed', 'Deaths', 'Recovered','Active',
+                'Incidence_Rate', 'Case-Fatality_Ratio']]
+
+    # One hot encoding for categorical values
+    category_clean = pd.get_dummies(all_data)
+
+    # Attach outcome column back
+    category_clean['outcome'] = df['outcome']
+    category_clean['age_filled'] = df['age_filled']
+    category_clean['filled_sex'] = df['filled_sex']
+    category_clean['province_filled'] = df['province_filled']
+    category_clean['country_filled'] = df['country_filled']
+
+    print("Splitting data into training and validation sets")
+    train, validate = train_test_split(category_clean, test_size=0.2, random_state=42, shuffle=True)
+
+
+    train_attr = train.drop(columns=['outcome', 'age_filled','filled_sex','province_filled','country_filled']) # Features
+    train_outcomes = train[['outcome']]
+
+    v_data = validate.drop(columns=['outcome', 'age_filled','filled_sex','province_filled','country_filled'])
+    v_outcomes = validate[['outcome']]
+    # 2.2 Training Model
+    print("Training Random Forests")
+    RandomForests.rf_train(train_attr, train_outcomes, param_grid)
+
+    """
+    # 2.3 Evaluate performance
+    print("Evaluating Random Forests Training")
+    RandomForests.rf_eval(train_attr,train_outcomes, True)
+
+    print("Evaluating Random Forests Validation")
+    RandomForests.rf_eval(v_data,v_outcomes,False)
+
+    RandomForests.investigate_deaths(validate)
+
+    # 2.4 Vary hyperparameter and check for overfitting
+    train_scores = []
+    validation_scores = []
+    depth_values = range(10,110,10)
+    for depth in depth_values:
+         print("Training RandomForest with depth", depth)
+         clf = RandomForests.overfit_rf_train(train_attr, train_outcomes, depth)
+         train_accuracy = RandomForests.overfit_eval(train_attr, train_outcomes, clf)
+         train_scores.append(train_accuracy)
+
+         validation_accuracy = RandomForests.overfit_eval(v_data, v_outcomes, clf)
+         validation_scores.append(validation_accuracy)
+    plt.figure()
+    plt.plot(depth_values, train_scores)
+    plt.plot(depth_values, validation_scores)
+    plt.title("Accuracy vs Max Depth Hyperparameter for Random Forests")
+    plt.ylabel("Accuracy")
+    plt.xlabel("Max Depth Hyperparameter")
+    plt.legend(['training scores', 'validation scores'])
+    plt.savefig("../plots/overfitting_check_rf.png")
+
+
+
+def light_gbm(df):
+    X = df[['age_filled', 'filled_sex', 'province_filled',
+                'country_filled','Confirmed', 'Deaths', 'Recovered','Active',
+                'Incidence_Rate', 'Case-Fatality_Ratio']]
+    y = df[['outcome']]
+    le = LabelEncoder()
+    le.fit(y)
+    y_encoded = le.transform(y)
+
+    X_train, X_valid, y_train, y_valid = train_test_split(X, y_encoded, test_size=0.2, random_state=42, shuffle=True)
+
+    # 2.2 Train Model
+    LightGbm.boosted_train(X_train, y_train, 8)
+
+    # 2.3 Evaluate performance
+    LightGbm.boosted_eval(X_train, y_train, le, True, True)
+    LightGbm.boosted_eval(X_valid, y_valid, le, True, False)
+
+    # Find feature importance
+    LightGbm.boosted_feature_importance(X_train)
+
+    # 2.4 Vary hyperparameter and check for overfitting
+    train_scores = []
+    validation_scores = []
+    depth_values = range(2,20,2)
+    for depth in depth_values:
+        print("Training LightGBM with depth", depth)
+        LightGbm.boosted_train(X_train, y_train, depth)
+        train_accuracy = LightGbm.boosted_eval(X_train, y_train, le, False)
+        train_scores.append(train_accuracy)
+
+        validation_accuracy = LightGbm.boosted_eval(X_valid, y_valid, le, False)
+        validation_scores.append(validation_accuracy)
+    plt.figure()
+    plt.plot(depth_values, train_scores)
+    plt.plot(depth_values, validation_scores)
+    plt.title("Accuracy vs Max Depth Hyperparameter for LightGBD")
+    plt.ylabel("Accuracy")
+    plt.xlabel("Max Depth Hyperparameter")
+    plt.legend(['training scores', 'validation scores'])
+    plt.savefig("../plots/overfitting_check_gbd.png", bbox_inches = "tight")
+
+"""
+if __name__ == '__main__':
+    main()
---
+++ Milestone3/src/RandomForests.py
@@ -0,0 +1,148 @@
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, recall_score, f1_score,precision_recall_fscore_support
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.metrics import make_scorer
+import seaborn as sns
+import pickle
+import matplotlib.pyplot as plt
+
+filename = '../models/rf_classifier.pkl'
+
+def f1Deceased(val, predict):
+    ans = precision_recall_fscore_support(val,predict, labels=['deceased'])
+    return ans[2]
+
+def recallDeceased(val, predict):
+    ans = precision_recall_fscore_support(val,predict, labels=['deceased'])
+    return ans[1]
+
+def rf_train(train_attr, train_outcomes, param_grid):
+
+    scoring = {
+            'f1_deceased': make_scorer(f1Deceased),
+            'recall_deceased': make_scorer(recallDeceased),
+            'accuracy': make_scorer(accuracy_score),
+            'recall': make_scorer(recall_score, average = 'micro')}
+
+
+    clf = RandomForestClassifier(random_state=42)
+
+    random_search = RandomizedSearchCV(clf, param_grid, n_iter=15,
+                                        scoring=scoring, refit='accuracy', verbose=10, cv=5,
+                                        n_jobs=1, random_state=42)
+
+
+    # Train model and write to file
+    random_search.fit(train_attr, train_outcomes)
+    all_results = random_search.cv_results_
+
+    for i in range(0,len(all_results['params'])):
+        print("Combination: ", end="")
+        print(all_results['params'][i])
+        print("F1 deceased score: ", end="")
+        print(all_results['mean_test_f1_deceased'][i])
+        print("Recall deceased score: ", end="")
+        print(all_results['mean_test_recall_deceased'][i])
+        print("Overall accuracy score: ", end="")
+        print(all_results['mean_test_accuracy'][i])
+        print("Overall recall score: ", end="")
+        print(all_results['mean_test_recall'][i])
+        print()
+
+
+    print(random_search.best_params_)
+    print(random_search.best_score_)
+    # clf.fit(train_attr, train_outcomes)
+    # pickle.dump(clf, open(filename, 'wb'))
+
+
+def overfit_rf_train(train_attr, train_outcomes, estimators):
+    clf = RandomForestClassifier(n_estimators=estimators)
+    # Train model and write to file
+    clf.fit(train_attr, train_outcomes)
+    return clf
+
+def overfit_eval(data,outcomes, clf):
+    predictions = clf.predict(data)
+    value = accuracy_score(outcomes, predictions)
+    return value
+
+def rf_eval(data, outcomes, name):
+    clf_load = pickle.load(open(filename, 'rb'))
+    predictions = clf_load.predict(data)
+    value = accuracy_score(outcomes, predictions)
+    print("Accuracy Score: ", end=" ")
+    print(value)
+
+'''
+    # Confusion Matrix
+    # Modified from https://medium.com/analytics-vidhya/evaluating-a-random-forest-model-9d165595ad56
+    matrix = confusion_matrix(outcomes, predictions)
+    matrix = matrix.astype('float') / matrix.sum(axis=1)[:, np.newaxis]
+
+    plt.figure(figsize=(16,7))
+    sns.set(font_scale=1.4)
+    sns.heatmap(matrix, annot=True, annot_kws={'size':10},
+                cmap=plt.cm.Blues, linewidths=0.2)
+
+    class_names = np.unique(outcomes['outcome'])
+    tick_marks = np.arange(len(class_names))
+    tick_marks2 = tick_marks + 0.5
+    plt.xticks(tick_marks, class_names, rotation=25)
+    plt.yticks(tick_marks2, class_names, rotation=0)
+    plt.xlabel('Predicted label')
+    plt.ylabel('True label')
+    plt.title('Confusion Matrix for Random Forest Model')
+    if(name):
+        plt.title('Confusion Matrix for Random Forest Model - Train Data')
+        plt.savefig("../plots/confusion_matrix_train_rf.png", bbox_inches = "tight")
+    else:
+        plt.title('Confusion Matrix for Random Forest Model - Validation Data')
+        plt.savefig("../plots/confusion_matrix_val_rf.png", bbox_inches = "tight")
+    #
+
+    # F1-scores
+    print(classification_report(outcomes, predictions))
+'''
+
+def investigate_deaths(validate):
+    clf_load = pickle.load(open(filename, 'rb'))
+
+    feature_list = ['age_filled', 'filled_sex', 'province_filled',
+                'country_filled','Confirmed', 'Deaths', 'Recovered','Active',
+                'Incidence_Rate', 'Case-Fatality_Ratio']
+
+     # feature importance adapted from https://www.kaggle.com/ashishpatel26/feature-importance-of-lightgbm
+    feature_imp = pd.DataFrame(sorted(zip(clf_load.feature_importances_, feature_list)),columns=['Value','Feature'])
+    plt.figure(figsize=(20, 10))
+    sns.barplot(x="Value", y="Feature", data=feature_imp.sort_values(by="Value", ascending=False))
+    plt.title('Random Forest Features Importance')
+    plt.tight_layout()
+    plt.savefig('../plots/feature_importances_rf.png')
+
+    deaths = validate[validate['outcome'] == 'deceased']
+    hospitalized = validate[validate['outcome'] == 'hospitalized']
+    hospitalized_avgs = {'age': hospitalized['age_filled'].mean(), 'gender': hospitalized['filled_sex'].mode()[0],
+                         'province_filled': hospitalized['province_filled'].mode()[0],
+                         'country_filled': hospitalized['country_filled'].mode()[0], 'Confirmed': hospitalized['Confirmed'].mode()[0],
+                         'Deaths': hospitalized['Deaths'].mode()[0], 'Recovered': hospitalized['Recovered'].mode()[0],
+                         'Incidence_Rate': hospitalized['Incidence_Rate'].mode()[0],
+                         'Active': hospitalized['Active'].mode()[0]}
+
+    overallCount = 0
+    for idx, row in deaths.iterrows():
+        matches = 0
+        if row['province_filled'] == hospitalized_avgs['province_filled']:
+            matches+=1
+        if row['Recovered'] >= hospitalized_avgs['Recovered'] - 10000 and row['Recovered'] <= hospitalized_avgs['Recovered'] + 10000:
+            matches +=1
+        if row['Deaths'] >= hospitalized_avgs['Deaths'] - 1000 and row['Deaths'] <= hospitalized_avgs['Deaths'] + 1000:
+            matches +=1
+        if row['Active'] >= hospitalized_avgs['Active'] - 10000 and row['Active'] <= hospitalized_avgs['Active'] + 10000:
+            matches +=1
+        if matches > 3:
+            overallCount+=1
+
+    print('Similarity between deceased and hospitalized: ', end=" ")
+    print(round(overallCount/len(deaths['outcome']), 2))
